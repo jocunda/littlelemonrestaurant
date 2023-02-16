f734768592771369df7a4b10a3bb8c709f37a59f
jocunda <silviawanshan88@gmail.com>
2023-02-16 13:56:45 +0800

    add gif

--- src/hooks/useSubmit.js
+++ src/hooks/useSubmit.js
@@ -2,32 +2,63 @@ import { useState } from "react";
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const fetchData = async () => {
-    try {
-        const response = await fetch('https://raw.githubusercontent.com/Meta-Front-End-Developer-PC/capstone/master/api.js');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
+
+const seededRandom = function (seed) {
+    var m = 2 ** 35 - 31;
+    var a = 185852;
+    var s = seed % m;
+    return function () {
+        return (s = s * a % m) / m;
+    };
+}
+
+const fetchAPI = function (date) {
+    let result = [];
+    let random = seededRandom(date.getDate());
+
+    for (let i = 17; i <= 23; i++) {
+        if (random() < 0.5) {
+            result.push(i + ':00');
+        }
+        if (random() < 0.5) {
+            result.push(i + ':30');
+        }
     }
+    return result;
+};
+const submitAPI = function (formData) {
+    return true;
 };
 
 const useSubmit = () => {
     const [loading, setLoading] = useState(false);
     const [response, setResponse] = useState(null);
 
+
     const submit = async (url, data) => {
-        const random = Math.random();
         setLoading(true);
         try {
             await wait(2000);
-            if (false) {
-                throw new Error("Something went wrong");
+            const bookingSlots = fetchAPI(new Date());
+            if (bookingSlots.length > 0) {
+                const success = submitAPI(data);
+                if (success) {
+                    setResponse({
+                        type: 'success',
+                        message: `Thanks for your submission, we will get back to you shortly!`,
+                    });
+                } else {
+                    setResponse({
+                        type: 'error',
+                        message: 'Booking submission failed.',
+                    });
+                }
+            } else {
+                setResponse({
+                    type: 'error',
+                    message: 'No available booking slots.',
+                });
             }
-            setResponse({
-                type: 'success',
-                message: `Thank you ${data.name}, booking was successfully submitted!`,
-            })
 
         } catch (error) {
             setResponse({
